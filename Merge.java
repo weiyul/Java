d13ce639bb9a2e80ae13a60895f606d8af3b6881
Wei-Yu Liao <sofialiao@hotmail.com>
2017-01-19 15:47:36 -0800

    Interview Question

---
+++ Merge.java
@@ -0,0 +1,15 @@
+
+// From Cracking the Coding Interview 4th edition
+// Solution 9.1
+// Question: You are given two sorted arrays, A and B, and A has a large enough buffer at the end to hold B. Write a method to merge B into B in sorted order.
+public class Merge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void merge(int[] a){
+		
+	}
+
+}
