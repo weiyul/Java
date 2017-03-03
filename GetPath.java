ae83af560a601ba2f3613e9d449a02b127954f1a
Wei-Yu Liao <sofialiao@hotmail.com>
2017-03-02 16:28:49 -0800

    Add files via upload

---
+++ GetPath.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+
+// Solution from Cracking the Coding Interview 4th
+// There was no content of isFree(), so I complete it
+// 9.2 Imagine a robot sitting on the upper left corner of an X by Y grid. The robot can only move in two directions: right and down. How many possible paths are there for the robot to go from (0,0) to (X, Y)?
+// FOLLOW UP
+// Imagine certain spots are "off limits," such that the robot cannot step on them.
+// Design an algorithm to find a path for the robot from the top left to the bottom right.
+
+public class GetPath {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		GetPath Gp=new GetPath();
+		ArrayList<Point> path = new ArrayList<>();
+		Point p1= new Point(0,0);
+		Point p2 = new Point(1,0);
+		Point p3 = new Point(0,1);
+		Point p4 = new Point(1,1);
+		Point p5 = new Point(2,0);
+		Point p6 = new Point(3,0);
+		
+		path.add(p1);
+		path.add(p2);
+		path.add(p3);
+		path.add(p4);
+		path.add(p5);
+		path.add(p6);
+
+		System.out.println("Show points on path");
+		for(Point p: path){
+			System.out.println("("+p.getX()+", "+p.getY()+")");
+		
+		}
+	
+		System.out.println("final result: "+Gp.getPath(3, 0, path));
+
+
+	}
+	
+	// To find a path from the origin, we just work backwards like this. Starting from the last cell, we try to find a path to each of its adjacent cells.
+	// insert end point (x, y)
+	// getPath() is to check if there are any path to (x,y)
+	public boolean getPath(int x, int y, ArrayList<Point> path){
+		Point p = new Point(x,y);
+		path.add(p);
+		if(x==0 && y==0){
+			return true; //find a path
+		}
+		boolean success = false;
+		if(x>=1 && isFree(x-1,y)){// Try right
+			success = getPath(x-1,y, path);// free, go right
+		}
+		if(!success && y>=1 && isFree(x, y-1)){ //Try down
+			success = getPath(x, y-1, path);// free, go down
+		}
+		if(!success){
+			
+			path.remove(p); //Wrong way, better stop going this way
+			System.out.println("remove ("+p.getX()+", "+p.getY()+")");
+			System.out.println("Show points on path after remove p");
+			for(Point pp: path){
+				System.out.println("("+pp.getX()+", "+pp.getY()+")");
+			
+			}
+			
+		}
+		System.out.println("show result inside getPath: "+success+ " for ("+p.getX()+", "+p.getY()+")");
+
+		
+		
+		return success;
+	}
+	
+	//check the if x>=0, y>=0, or encounter any unaccess point
+	private boolean isFree(int x, int y){
+		boolean free=true;
+		Point unAcessP1 = new Point(1,0);
+		if(x <0 || y<0 ){
+			free =false;
+		}
+		if(x== unAcessP1.getX() && y== unAcessP1.getY() ){
+			free = false;
+		}
+			
+		
+		
+		return free;
+	}
+
+}
