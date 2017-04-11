f87738937b58743c59bdff7e1414eddf620f8a09
Wei-Yu Liao <sofialiao@hotmail.com>
2017-04-11 16:06:47 -0700

    Add files via upload

---
+++ AJAX_Example_JavaScript.js
@@ -0,0 +1,35 @@
+function getXMLHttp(){
+	var xmlHttp;
+	try{
+		xmlHttp = new XMLHttpRequest();
+	}catch(e){
+		//Internet Explorer is different than the others
+		try{
+			xmlHttp = new ActiveXObject("Msxm12.XMLHTTP");
+		}catch(e){
+			try{
+				xmlHttp= new ActiveXObject("Microsoft.XMLHTTP"); 
+			}catch(e){
+				alert("old browser? Upgrade today so you can use AJAX");
+				return false;
+			}
+		}
+	}
+	return xmlHttp;
+}
+
+function AjaxRequest(){
+	var xmlHttp = getXMLHttp();
+	xmlHttp.onreadystatechange = function(){
+		if(xmlHttp.readyState == 4){
+			HandleResponse(xmlHttp.responseText);
+		}
+	}
+	xmlHttp.open("GET", "myfirstprogram.php", true);
+
+	xmlHttp.send(null);
+}
+
+function HandleResponse(response){
+	document.getElementById("AjaxResponse").innerHTML = response;
+}
